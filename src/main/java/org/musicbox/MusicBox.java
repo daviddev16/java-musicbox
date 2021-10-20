c47a91488454ebce202b49268726a1458fcf3358
David <37241920+daviddev16@users.noreply.github.com>
2021-10-20 02:28:44 -0300

    .

--- src/main/java/org/musicbox/MusicBox.java
+++ src/main/java/org/musicbox/MusicBox.java
@@ -81,7 +81,7 @@ public class MusicBox {
             .setGatewayEncoding(GatewayEncoding.ETF)
             .setChunkingFilter(ChunkingFilter.NONE)
             .setMemberCachePolicy(MemberCachePolicy.VOICE)
-            .addEventListeners(Listeners.getAllListeners())
+            .addEventListeners(Arrays.asList(Listeners.getAllListeners()))
             .setRawEventsEnabled(true)
             .setBulkDeleteSplittingEnabled(false)
             .setEventPool(ThreadingConfig.newScheduler(1, () -> "MusicBox", "EventPool"), true)
